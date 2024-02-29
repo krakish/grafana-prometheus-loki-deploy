d367bd0e162c49c21947a556aed45d0a2903fdf5
Andrei <ipatovichaa@gmail.com>
2024-02-29 10:55:38 -0500

    Add terraform infrastructure

---
+++ terraform-infra/droplet-1-server.tf
@@ -0,0 +1,46 @@
+resource "digitalocean_firewall" "droplet-1-firewall" {
+  name        = "droplet-1-firewall"
+  droplet_ids = [digitalocean_droplet.droplet-1.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "5044"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_tag" "droplet-1-tag" {
+  name = "droplet-1"
+}
+
+
+resource "digitalocean_droplet" "droplet-1" {
+  image      = "ubuntu-22-04-x64"
+  name       = "graylog-server"
+  region     = "fra1"
+  size       = "s-2vcpu-4gb"
+  vpc_uuid   = resource.digitalocean_vpc.test-vpc.id
+  ssh_keys   = [data.digitalocean_ssh_key.wind-key.id]
+  user_data  = file("./server-user-data-docker.sh")
+  monitoring = true
+  tags       = [digitalocean_tag.droplet-1-tag.id]
+}
+
