8a93e2193a49326bba5d88e128e2beef17d14313
Andrei <ipatovichaa@gmail.com>
2024-03-16 12:48:34 -0400

    Edit firewall

--- terraform-infra/droplet-1-server.tf
+++ terraform-infra/droplet-1-server.tf
@@ -14,9 +14,20 @@ resource "digitalocean_firewall" "droplet-1-firewall" {
     source_addresses = ["0.0.0.0/0"]
   }
 
+    inbound_rule {
+    protocol         = "udp"
+    port_range       = "53"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
